e40016dd5aa9b88af507bcc3f4888b5d04109e24
Kartonagnick <CastleOfDreams@yandex.ru>
2021-05-20 18:05:00 +0300

    upd: test-develop.hpp (002) PRE

--- test/include/test-develop.hpp
+++ test/include/test-develop.hpp
@@ -1,3 +1,4 @@
+// [2020y-05m-20d][14:30:00] Idrisov Denis R. 002 PRE
 // [2020y-05m-19d][23:00:00] Idrisov Denis R. 001
 #pragma once
 #ifndef dTEST_DEVELOP_USED_ 
@@ -7,6 +8,12 @@
 //===== modern/classic =================||==================||==================
     #define TEST_MODERN                 001                 // ready!
     #define TEST_CLASSIC                001                 // ready!
+//======================================||==================||==================
+//===== tools/features =================||==================||==================
+    #define TEST_ATOMIC                 001                 // ready!
+    #define TEST_STASTIC_CHECK          001                 // ready!
+    #define TEST_NOEXCEPT               001                 // ready!
+    #define TEST_NOCOPYABLE             001                 // ready!
 //==============================================================================
 //==============================================================================
 // in progress...
