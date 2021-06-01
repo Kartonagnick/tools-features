85f30891dbc7388123c3434b7f7fba1fd5c176af
Kartonagnick <CastleOfDreams@yandex.ru>
2021-06-01 12:04:30 +0300

    upd: features.hpp (003) PRE

--- include/tools/features.hpp
+++ include/tools/features.hpp
@@ -1,23 +1,80 @@
+// [2020y-06m-01d][11:15:00] Idrisov Denis R. 003 PRE
 // [2020y-05m-21d][02:00:00] Idrisov Denis R. 002
 // [2020y-05m-19d][23:00:00] Idrisov Denis R. 001
 #pragma once
 #ifndef dTOOLS_FEATURES_USED_
-#define dTOOLS_FEATURES_USED_ 2
+#define dTOOLS_FEATURES_USED_ 3
 
 //==============================================================================
-//=== dHAS_ATOMIC ==============================================================
+//=== dHAS_NULLPTR =============================================================
 
-#if !defined(_MSC_VER) || _MSC_VER >= 1700
-    // msvc2012 (or newer)
-    #define dHAS_ATOMIC 1
+#if defined(__GNUC__) && __GNUC__ * 10 + __GNUC_MINOR__ >= 46
+    #define dHAS_NULLPTR 1
 #endif
+
+#if __cplusplus >= 201103L
+    #define dHAS_NULLPTR 1
+#endif
+
+#ifdef dHAS_NULLPTR
+    typedef decltype(nullptr) nullptr_t;
+#endif
+
+//==============================================================================
+//=== msvc2010 =================================================================
 
 #if !defined(_MSC_VER) || _MSC_VER >= 1600
     // msvc2010 (or newer)
 
+    // [WARNING] msvc2010 has bug with reference
+    // bug: can be "std::tr1::_Remove_reference<_Ty&&>"
+    // bug: or "std::tr1::_Remove_reference<_Ty&>"
+    #define dHAS_TYPE_TRAITS 1
+
+    #define dHAS_DECLTYPE 1
+    #define dHAS_NULLPTR 1
+    #define dHAS_LAMBDA 1
+    #define dHAS_HASH 1
+#endif
+
+//==============================================================================
+//=== dTRAIT ===================================================================
+
+#ifdef dHAS_TYPE_TRAITS
+    #define dTRAIT std
+#else
+    #define dTRAIT tools
+#endif
+
+//==============================================================================
+//=== dSTATIC_ASSERT ===========================================================
+
+#if !defined(_MSC_VER) || _MSC_VER >= 1600
+    // msvc2010 (or newer)
+    #define dHAS_CSTDINT 1
+    #define dHAS_STATIC_ASSERT 1
+
+    #define dSTATIC_ASSERT(msg, ...) \
+        static_assert((__VA_ARGS__), #msg)
+
     #define dSTATIC_CHECK(msg, ...)  \
         static_assert((__VA_ARGS__), #msg)
+
 #else
+
+    #define dSTATIC_ASSERT_3(msg, ...) \
+        struct msg { int static_assert_ : !!(__VA_ARGS__); }
+
+    #define dSTATIC_ASSERT_2(msg, L, C, ...)  \
+        dSTATIC_ASSERT_3(msg##_##L##_##C , __VA_ARGS__)
+
+    #define dSTATIC_ASSERT_1(msg, L, C, ...) \
+        dSTATIC_ASSERT_2(msg, L, C, __VA_ARGS__)
+
+    #define dSTATIC_ASSERT(msg, ...) \
+        dSTATIC_ASSERT_1(msg, __LINE__, __COUNTER__, __VA_ARGS__)
+
+    
     template<bool> struct static_assert_;
     template<> struct static_assert_<true> {};
 
@@ -26,6 +83,15 @@
         ::static_assert_<(__VA_ARGS__)> msg; \
         (void) msg;                          \
     } void()
+
+#endif
+
+//==============================================================================
+//=== dHAS_ATOMIC ==============================================================
+
+#if !defined(_MSC_VER) || _MSC_VER >= 1700
+    // msvc2012 (or newer)
+    #define dHAS_ATOMIC 1
 #endif
 
 //==============================================================================
@@ -78,4 +144,4 @@
 
 //==============================================================================
 //==============================================================================
-#endif // !dTOOLS_FEATURES_USED_
+#endif // dTOOLS_FEATURES_USED_
